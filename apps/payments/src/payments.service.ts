0cbb348c0d5fe32077de02bc958472aa392a4764
glaudistontheonly <glaudistonson@gmail.com>
2023-10-15 18:05:05 -0300

    ➕ added payments microservice

---
+++ apps/payments/src/payments.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { CreateChargeDto } from './dto/create-charge.dto';
+import Stripe from 'stripe';
+
+@Injectable()
+export class PaymentsService {
+  constructor(private readonly configService: ConfigService) {
+    
+  }
+  private readonly stripe = new Stripe(this.configService.get('STRIPE_SECRET_KEY'),
+  {
+    apiVersion: '2023-08-16',
+  }
+  );
+  
+  async createCharge( { card, amount }: CreateChargeDto
+  ) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      amount: amount * 100,
+      currency: 'usd',
+      payment_method: paymentMethod.id,
+      confirm: true,
+      payment_method_types: ['card'],
+    });
+
+    return paymentIntent;
+  }
+}
--- apps/reservations/src/reservations.controller.ts
+++ apps/reservations/src/reservations.controller.ts
@@ -17,21 +17,25 @@ export class ReservationsController {
     return this.reservationsService.create(createReservationDto, user._id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async findAll() {
     return this.reservationsService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.reservationsService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateReservationDto: UpdateReservationDto) {
     return this.reservationsService.update(id, updateReservationDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return this.reservationsService.remove(id);
