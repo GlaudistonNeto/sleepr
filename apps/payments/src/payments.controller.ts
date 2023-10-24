654c53d4b62e0706cd5a96a8b43496744f4c0ed9
glaudistontheonly <glaudistonson@gmail.com>
2023-10-24 17:23:19 -0300

    🛠 refactoring changes

--- apps/payments/src/payments.controller.ts
+++ apps/payments/src/payments.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { PaymentsService } from './payments.service';
+import { Controller, UsePipes, ValidationPipe } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { CreateChargeDto } from './dto/create-charge.dto';
+import { CreateChargeDto } from '@app/common';
+import { PaymentsService } from './payments.service';
 
 @Controller()
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
   @MessagePattern('create_charge')
+  @UsePipes(new ValidationPipe())
   async createCharge(@Payload() data: CreateChargeDto) {
     return this.paymentsService.createCharge(data);
   }
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,41 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { CreateChargeDto } from './dto/create-charge.dto';
 import Stripe from 'stripe';
+import { CreateChargeDto } from '../../../libs/common/src/dto/create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
-  constructor(private readonly configService: ConfigService) {
-    
-  }
-  private readonly stripe = new Stripe(this.configService.get('STRIPE_SECRET_KEY'),
-  {
-    apiVersion: '2023-08-16',
-  }
+  private readonly stripe = new Stripe(
+    this.configService.get('STRIPE_SECRET_KEY'),
+    {
+      apiVersion: '2023-08-16',
+    },
   );
-  
-  async createCharge( { card, amount }: CreateChargeDto
-  ) {
-    const paymentMethod = await this.stripe.paymentMethods.create({
-      type: 'card',
-      card,
-    });
 
+  constructor(private readonly configService: ConfigService) {}
+
+  async createCharge({ card, amount }: CreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
-<<<<<<< Updated upstream
-=======
       automatic_payment_methods: {
         enabled: true,
         allow_redirects: 'never',
       },
       confirm: true,
       payment_method: 'pm_card_visa',
->>>>>>> Stashed changes
       currency: 'usd',
-      payment_method: paymentMethod.id,
-      confirm: true,
-      payment_method_types: ['card'],
     });
 
     return paymentIntent;
